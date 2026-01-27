a71b4d587d986986c30e0a96ead32e88b5f0ee2b
DavidB <david.bernard@fms-ea.com>
2026-01-27 15:40:45 +0100

    route "" ok + tableau ok avec les formations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 import { TrainingComponent } from './components/trainings/trainings';
 
 export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/trainings',
+        pathMatch: 'full' 
+    },
    
     { path: 'trainings', 
     component: TrainingComponent 
