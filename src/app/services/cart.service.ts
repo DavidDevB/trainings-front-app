2ed7fd19050ddca04befa0494ea450bd1910fa1f
DavidB <david.bernard@fms-ea.com>
2026-01-28 11:31:46 +0100

    possibilité d'afficher le panier OU les trainings selon le click

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -7,9 +7,14 @@ import { Training } from '../models/training.model';
 export class CartService {
 
   private trainingsInCart: Training[] = [];
+  showCart: boolean = false;
   
   constructor() {
 
+  }
+
+  displayTrainingsInCart(): void {
+    this.showCart = true;
   }
 
   addToCart(training: Training) {
@@ -22,6 +27,10 @@ export class CartService {
 
   getTotalPrice(): number {
     return this.trainingsInCart.reduce((total, training) => total + (training.price * training.quantity), 0);
+  }
+
+  removeFromCart(trainingId: number) {
+    this.trainingsInCart = this.trainingsInCart.filter(training => training.id !== trainingId);
   }
 
 }
