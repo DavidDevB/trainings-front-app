cb3b1d566973d49ffeb2fecb7cf8eba06b2a0a80
DavidB <david.bernard@fms-ea.com>
2026-02-05 09:36:34 +0100

    file name changed with a dot

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Training } from '../models/training.model';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http:HttpClient) {}
+
+  public getTrainings() {
+    return this.http.get<Training[]>('http://localhost:3000/trainings');
+  }
+
+  public getUsers() {
+    return this.http.get<any[]>('http://localhost:3000/users');
+  }
+}
